dd1ff358c5d763842e43ef31a75ef181cca63898
Rodrigo <rodrigo.silva96@outlook.pt>
2020-09-25 18:55:58 +0100

    Started project, defined contract in proto

---
+++ DIDA-GSTORE/DIDA-GSTORE/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace DIDA_GSTORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = args[0];
+            // process input file ...
+
+
+        }
+    }
+}
---
+++ DIDA-GSTORE/PupperMaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PupperMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
